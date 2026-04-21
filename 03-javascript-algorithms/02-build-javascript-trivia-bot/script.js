1c7318d7f235f37161c9819bdcc1545bcd6ecfa2
Code Mate <dev.codemate@gmail.com>
2026-04-21 16:01:56 -0300

    feat: add javascript trivia bot exercise

---
+++ 03-javascript-algorithms/02-build-javascript-trivia-bot/script.js
@@ -0,0 +1,20 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+const botName = "Flo";
+const botLocation = "La Plata";
+const favoriteLanguage = "JavaScript";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = "My favorite programming language is " + favoriteLanguage + ".";
+console.log(codingFact);
+
+codingFact = "I started learning " + favoriteLanguage + " and I love it.";
+console.log(codingFact);
+
+codingFact = "I build projects using " + favoriteLanguage + ".";
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
