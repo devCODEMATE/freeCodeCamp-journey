ff4a5af826d4988f1ff24e93a91c35768a9124f5
Code Mate <dev.codemate@gmail.com>
2026-04-21 16:13:52 -0300

    feat: add sentence maker exercise

---
+++ 03-javascript-algorithms/03-build-sentence-maker/script.js
@@ -0,0 +1,25 @@
+// Variables para la primera historia
+let adjective = "brave";
+let noun = "dragon";
+let verb = "angry";
+let place = "mountain";
+let adjective2 = "fiery";
+let noun2 = "cookies";
+
+// Primera historia
+let firstStory = `Once upon a time, there was a(n) ${adjective} ${noun} who loved to eat ${noun2}. The ${noun} lived in a ${place} and had ${adjective2} nostrils that blew fire when it was ${verb}.`;
+
+console.log("First story: " + firstStory);
+
+// Nuevos valores para la segunda historia
+adjective = "funny";
+noun = "robot";
+verb = "excited";
+place = "city";
+adjective2 = "sparkly";
+noun2 = "pizza";
+
+// Segunda historia
+let secondStory = `Once upon a time, there was a(n) ${adjective} ${noun} who loved to eat ${noun2}. The ${noun} lived in a ${place} and had ${adjective2} nostrils that blew fire when it was ${verb}.`;
+
+console.log("Second story: " + secondStory);
