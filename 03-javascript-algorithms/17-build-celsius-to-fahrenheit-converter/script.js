dbbe74769878a42e1c4de4f8dd36171ae010aec5
Code Mate <dev.codemate@gmail.com>
2026-05-05 11:31:31 -0300

    feat: add celsius to fahrenheit converter exercise

---
+++ 03-javascript-algorithms/17-build-celsius-to-fahrenheit-converter/script.js
@@ -0,0 +1,10 @@
+function convertCtoF(celsius) {
+  return celsius * (9 / 5) + 32;
+}
+
+console.log(convertCtoF(0));
+console.log(convertCtoF(-30));
+console.log(convertCtoF(-10));
+console.log(convertCtoF(0));
+console.log(convertCtoF(20));
+console.log(convertCtoF(30));
