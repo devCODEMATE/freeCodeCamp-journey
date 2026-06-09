131f84f9dcf47d65f981a3f0fce58f6f9b52ce73
Code Mate <dev.codemate@gmail.com>
2026-06-09 17:08:35 -0300

    feat: add longest word finder app exercise
    Implement a function to find the length of the longest word in a string.

---
+++ 03-javascript-algorithms/34-build-longest-word-finder-app/script.js
@@ -0,0 +1,23 @@
+function findLongestWordLength(str) {
+  const words = str.split(" ");
+  let longest = 0;
+
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].length > longest) {
+      longest = words[i].length;
+    }
+  }
+
+  return longest;
+}
+
+
+
+console.log(findLongestWordLength("The quick brown fox jumped over the lazy dog"));
+console.log(findLongestWordLength("The quick brown fox jumped over the lazy dog"));
+console.log(findLongestWordLength("May the force be with you"));
+console.log(findLongestWordLength("Googling do a barrel roll"));
+console.log(findLongestWordLength("Googling do a barrel roll"));
+console.log(findLongestWordLength("What is the average airspeed velocity of an unladen swallow"));
+console.log(findLongestWordLength("What if we try a super-long word such as otorhinolaryngology"));
+
