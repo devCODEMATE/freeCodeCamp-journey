c30f441c52f32603898188819c454d73071bcbe3
Code Mate <dev.codemate@gmail.com>
2026-06-11 18:55:08 -0300

    feat: add slice and splice algorithm exercise

---
+++ 03-javascript-algorithms/47-slice-and-splice-algorithm/script.js
@@ -0,0 +1,9 @@
+function frankenSplice(arr1, arr2, n) {
+  const copy = arr2.slice();      // copia para no mutar arr2
+  copy.splice(n, 0, ...arr1);     // inserta arr1 dentro de copy desde índice n
+  return copy;
+}
+
+console.log(frankenSplice([1, 2, 3], [4, 5], 1));
+console.log(frankenSplice([1, 2], ["a", "b"], 1));
+console.log(frankenSplice(["claw", "tentacle"], ["head", "shoulders", "knees", "toes"], 2));
